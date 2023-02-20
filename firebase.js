f911a173eb3032aba4427a27dd808ac09528d816
Usman Aslam <90748447+UsmanRepos@users.noreply.github.com>
2023-02-20 16:11:47 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,13 +6,13 @@ import * as firebase from 'firebase/';
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzHYRYShfkg4KPymyHLva4Vxz2LleZVuI",
-  authDomain: "easyride-9c986.firebaseapp.com",
-  projectId: "easyride-9c986",
-  storageBucket: "easyride-9c986.appspot.com",
-  messagingSenderId: "825632191688",
-  appId: "1:825632191688:web:b2bc5cd7671247c50f21fd",
-  measurementId: "G-YQC7YZCHKW"
+  apiKey: "..........",
+  authDomain: "..........",
+  projectId: "..........",
+  storageBucket: "..........",
+  messagingSenderId: "..........",
+  appId: "..........",
+  measurementId: ".........."
 };
 
 // Initialize Firebase
